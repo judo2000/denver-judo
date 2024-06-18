cab37039b128ed1617a2ed8ca98b9ea13a852504
Stephen Moore <judo2000@comcast.net>
2024-06-18 11:55:54 -0600

    Install react-redux and redux toolkit

---
+++ client/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = import.meta.env.Dev
+  ? "http://localhost:5000"
+  : "https://denverjudo.com";
+export const USERS_URL = "/api/users";
+export const CONTENT_URL = "/api/content";
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,14 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import App from "./App.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
-// styles
+import { Provider } from "react-redux";
+import store from "./store.js";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
-
+import App from "./App.jsx";
 import "./index.css";
+
+import HomeScreen from "./screens/HomeScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +24,8 @@ const router = createBrowserRouter(
 );
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
